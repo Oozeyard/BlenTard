9befd9f13231284629080e18812a598a29d6d550
Galax416 <emmanuelfontaine416@gmail.com>
2025-01-06 13:11:38 +0100

    add vertices rotation and scale

--- src/app/widgets/GLWidget.h
+++ src/app/widgets/GLWidget.h
@@ -57,6 +57,7 @@ protected:
     void paintGL() override;
     void resizeGL(int w, int h) override;
     void keyPressEvent(QKeyEvent *event) override;
+    void keyReleaseEvent(QKeyEvent *event) override;
     void mousePressEvent(QMouseEvent *event) override;
     void mouseMoveEvent(QMouseEvent *event) override;
     void mouseReleaseEvent(QMouseEvent *event) override;
@@ -112,7 +113,8 @@ private:
     Transform m_initialNodeTransform, m_lastNodeTransform;
     std::atomic<int> m_activeTransforms{0}, m_nbTotalActiveTransform{0}; // Number of active transformations
 
-    // Selection (Color picking)  
+    // Selection (Color picking) 
+    bool m_isShitKeyPressed { false }; 
     GLPainter *m_glPainter;
     QRect m_selectedRect;
     void initializeSelectionBuffer();
--- src/includes/Utils.h
+++ src/includes/Utils.h
@@ -7,6 +7,8 @@
 QVector3D rayPlaneIntersection(const QVector3D& rayOrigin, const QVector3D& rayDirection, const QVector3D& planeOrigin, const QVector3D& planeNormal);
 void mouseToRay(const QPoint& mousePos, QVector3D& rayOrigin, QVector3D& rayDirection, const Camera* m_camera, const int width, const int height);
 QVector3D worldToScreen(const QVector3D& worldPos, const Camera* m_camera, const int width, const int height);
+QVector3D getMidlePoint(const QVector<QVector3D>& points);
+
 inline uint qHash(const QVector3D& key, uint seed = 0) {
     return qHash(key.x(), seed) ^ qHash(key.y(), seed << 1) ^ qHash(key.z(), seed << 2);
 }
